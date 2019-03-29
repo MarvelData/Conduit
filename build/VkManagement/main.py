3c6bdcc7562b457b64f6b6a34796f57588b43502
Marvel <ilya97@ziza.ru>
2019-03-29 18:03:24 +0300

    VK api integration V1.01 - fixes & improvements

--- build/VkManagement/main.py
+++ build/VkManagement/main.py
@@ -1,24 +1,29 @@
+import argparse
 import sys
 import vk
 
 from collections import defaultdict
+from datetime import datetime
+from matplotlib import pyplot
 from subprocess import Popen, PIPE
 from threading import Thread
+from time import sleep
 from queue import Queue, Empty
+
 
 class Vk:
     def __init__(self):
-        print('Initializing vk api')
+        print('\nInitializing vk api')
         self.api_version = '5.92'
         self.community_id = '17592208'
         self.user_api = None
         self.community_api = None
-        if len(sys.argv) > 2:
-            self.user_api = vk.API(vk.AuthSession('3116505', sys.argv[1], sys.argv[2], scope='wall, groups'))
+        if command_args.login and command_args.password:
+            self.user_api = vk.API(vk.AuthSession('3116505', command_args.login, command_args.password, scope='wall, groups'))
         else:
             print('Not enough data to initialize vk user api')
-        if len(sys.argv) > 3:
-            self.community_api = vk.API(vk.Session(access_token=sys.argv[3]))
+        if command_args.token:
+            self.community_api = vk.API(vk.Session(access_token=command_args.token))
         else:
             print('Not enough data to initialize vk community api')
 
@@ -33,8 +38,6 @@ class Vk:
         return self.community_api.messages.getHistory(peer_id=conversation['conversation']['peer']['id'], v=self.api_version)
 
     def print_conversations(self, id):
-        if not self.community_api:
-            return None
         conversations = self.get_conversations()
         for item in conversations['items']:
             messages = self.get_messages(item)
@@ -45,7 +48,11 @@ class Vk:
     def get_posts(self, offset=0, count=100, filter='all'):
         if not self.user_api:
             return None
-        return self.user_api.wall.get(owner_id='-' + self.community_id, v=self.api_version, offset=offset, count=count, filter=filter)
+        try:
+            return self.user_api.wall.get(owner_id='-' + self.community_id, v=self.api_version, offset=offset, count=count, filter=filter)
+        except:
+            sleep(0.5)
+            return self.get_posts(offset, count, filter)
 
     def get_post_info(self, offset = 0, amount = 1, filter = 'all', i = 0):
         if not self.user_api:
@@ -57,7 +64,7 @@ class Vk:
 
 class RegBook:
     def __init__(self, silent = True):
-        self.instance = Popen(["Regbook"], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+        self.instance = Popen(executable, stdout=PIPE, stdin=PIPE, stderr=PIPE)
         self.stdin = self.instance.stdin
         self.stdout = self.instance.stdout
         self.queue = Queue()
@@ -148,6 +155,15 @@ class RegBook:
             else:
                 lines.append(line)
 
+    def get_members_list(self, info=True):
+        if info:
+            members = self.ask('2')
+        else:
+            members = self.ask('1')
+        self.ask('-1')
+        self.ask('1')
+        return members
+
     def get_members_amount(self):
         members = self.ask('2')
         self.ask('-1')
@@ -163,6 +179,16 @@ class RegBook:
         data = self.ask(str(i))
         self.ask('1')
         return data
+
+    def get_member_data_safe(self, member_id):
+        self.ask('2')
+        data = self.ask(str(member_id))
+        for line in data:
+            if 'Short name' in line:
+                self.ask('1')
+                return data
+        self.instance.kill()
+        return None
 
     def find(self, request):
         self.ask('11')
@@ -181,21 +207,33 @@ class RegBook:
         self.ask(link)
         self.ask('1')
 
+
 class Tools:
     def __init__(self):
         self.vk = None
         self.funcs = []
         self.funcs.append(self.process_postponed_posts)
         self.funcs.append(self.process_posts)
-        self.funcs.append(self.get_stats)
+        self.funcs.append(self.get_member_stats)
+        self.funcs.append(self.get_member_plot)
+        self.funcs.append(self.get_members_plot)
         self.names = []
         self.names.append('I would like to autoadd posts for members')
         self.names.append('I would like to automanage posts')
-        self.names.append('I would like to get stats for members')
+        self.names.append('I would like to get stats for member')
+        self.names.append('I would like to get plot for member')
+        self.names.append('I would like to get plot for all members')
 
     def init_vk(self):
         if not self.vk:
             self.vk = Vk()
+        return self.vk.user_api
+
+    def my_date_to_universal(self, date):
+        return datetime.strptime(date, '%Y.%m.%d')
+
+    def vk_date_to_universal(self, date):
+        return datetime.fromtimestamp(date)
 
     def post_id_to_link(self, id):
         return 'https://vk.com/wall-17592208_' + id
@@ -210,24 +248,37 @@ class Tools:
     def rubric_to_hashtag(self, rubric):
         return '#MU_' + rubric
 
+    def clear_string(self, string):
+        return string.replace(' ', '').replace('\n', '').replace('\r', '')
+
     def list_to_string(self, list):
         string = ''
         for elem in list:
             string += elem + ', '
         return string[:-2]
 
+    def dates_list_to_plot_ticks(self, days, dates):
+        days_to_show, dates_to_show = [], []
+        last_shown_day = -sys.maxsize
+        for i, day_number in enumerate(days):
+            if day_number - last_shown_day > (days[-1] - days[0]) / 9:
+                last_shown_day = day_number
+                days_to_show.append(days[i])
+                dates_to_show.append(dates[i].date())
+        return days_to_show, dates_to_show
+
     def parse_member_data(self, data):
         member = {}
         for line in data:
             if 'Short name:' in line:
-                member['name'] = line.split(':')[1][1:-2]
+                member['name'] = self.clear_string(line.split(':')[1])
             if 'Rubric:' in line:
-                member['rubric'] = line.split(':')[1][1:-2]
+                member['rubric'] = self.clear_string(line.split(':')[1])
             if len(line) > 5 and not ' ' in line:
-                member['id'] = line[:-3]
+                member['id'] = self.clear_string(line.split('\\')[0])
         return member
 
-    def get_members_data(self, regBook):
+    def get_members_mapping(self, regBook):
         key_to_member = {}
         id_to_members = defaultdict(list)
         member_amount = regBook.get_members_amount()
@@ -237,14 +288,41 @@ class Tools:
             id_to_members[member['id']].append(member['name'])
         return key_to_member, id_to_members
 
+    def get_member_posts(self, data):
+        dates, days, amounts = [], [], []
+        date_from = self.my_date_to_universal('2019.01.01')
+        for line in data:
+            if 'http' in line:
+                columns = line.split(' ')
+                dates.append(self.my_date_to_universal(self.clear_string(columns[0])))
+                days.append((dates[-1] - date_from).days)
+                amounts.append(int(self.clear_string(columns[1])))
+        return dates, days, amounts
+
+    def choose_member(self, regBook, info=True):
+        members = regBook.get_members_list(info)
+        for line in members:
+            sys.stdout.write(line)
+        while True:
+            user_input = input()
+            if user_input == '-1':
+                return None, regBook
+            member_data = regBook.get_member_data_safe(user_input)
+            if not member_data:
+                regBook = RegBook()
+                print('\nNo such element! Try again.\n')
+            else:
+                break
+        for line in member_data[:-3]:
+            sys.stdout.write(line)
+        return member_data, regBook
+
     def process_postponed_posts(self):
-        self.init_vk()
-        regBook = RegBook()
-        key_to_member, id_to_members = self.get_members_data(regBook)
-        posts = self.vk.get_posts(0, 100, 'postponed ')
-        if not posts:
-            regBook.ask('-1')
+        if not self.init_vk():
             return
+        regBook = RegBook()
+        key_to_member, id_to_members = self.get_members_mapping(regBook)
+        posts = self.vk.get_posts(0, 100, 'postponed ')
         print('\nThere are', posts['count'], 'postponed posts\n')
         counter = 0
         not_found, need_work = [], []
@@ -273,41 +351,104 @@ class Tools:
         print(len(need_work), 'out of them have to be finished:\n')
         for post in need_work:
             print(post)
-        print('Silent RegBook returns', regBook.ask('-1'))
+        if regBook.ask('-1') != 0:
+            print('\nWarning: Silent RegBook did not exit correctly')
 
     def process_posts(self):
-        self.init_vk()
+        if not self.init_vk():
+            return
         print('\nprocess_posts')
 
-    def get_posts_stats(self, amount, id, rubric):
-        self.init_vk()
+    def get_member_stats(self):
+        if not self.init_vk():
+            return
+        regBook = RegBook()
+        member_data, regBook = self.choose_member(regBook, info=True)
+        if regBook.ask('-1') != 0:
+            print('\nWarning: Silent RegBook did not exit correctly')
+        if not member_data:
+            return
+        member = self.parse_member_data(member_data)
+        print('Input date to collect stats from in format YYYY.MM.DD\n')
+        date_from = self.my_date_to_universal(input())
+        id = int(member['id'])
+        rubric = self.rubric_to_hashtag(member['rubric'])
         stats = defaultdict(list)
         counter = 0
         offset = -100
-        while counter < amount:
+        date = datetime.now()
+        while date >= date_from:
             offset += 100
             posts = self.vk.get_posts(offset, 100)
-            if not posts:
-                return
             for post in posts['items']:
+                date = self.vk_date_to_universal(post['date'])
+                if date < date_from:
+                    break
                 if 'signer_id' in post and post['signer_id'] == id and self.get_hashtag(post) == rubric:
+                    print(date)
                     for stat_type in ['comments', 'likes', 'reposts', 'views']:
                         stats[stat_type].append(post[stat_type]['count'])
                     stats['efficiency'].append(post['likes']['count'] / post['views']['count'])
                     counter += 1
-                    if counter >= amount:
-                        break
+        print('\nThis member has', counter, 'posts since', date_from.date())
+        print('His average results are:')
         for stat_type, values in stats.items():
             sum = 0
             for value in values:
                 sum += value
-            print(stat_type, sum / counter)
+            print(stat_type + ':', sum / counter)
 
-    def get_stats(self):
-        print()
-        self.get_posts_stats(50, id, scenes)
+    def get_member_plot(self):
+        regBook = RegBook()
+        member_data, regBook = self.choose_member(regBook, info=True)
+        if regBook.ask('-1') != 0:
+            print('\nWarning: Silent RegBook did not exit correctly')
+        if not member_data:
+            return
+        dates, days, amounts = self.get_member_posts(member_data)
+        pyplot.xlabel('Dates')
+        pyplot.ylabel('Posts')
+        pyplot.grid(True)
+        pyplot.plot(days, amounts, 'g', linewidth='1')
+        pyplot.plot(days, amounts, 'bx', markersize='7')
+        days_to_show, dates_to_show = self.dates_list_to_plot_ticks(days, dates)
+        pyplot.xticks(days_to_show, dates_to_show)
+        pyplot.yticks(amounts, amounts)
+        pyplot.show()
 
-id = 356508400
-scenes = '#MU_Scenes'
+    def get_members_plot(self):
+        regBook = RegBook()
+        member_amount = regBook.get_members_amount()
+        pyplot.xlabel('Dates')
+        pyplot.ylabel('Posts')
+        pyplot.grid(True)
+        all_days, all_dates, all_amounts = [], [], []
+        for i in range(member_amount):
+            member_data = regBook.get_member_data(i)
+            dates, days, amounts = self.get_member_posts(member_data)
+            member = self.parse_member_data(member_data)
+            all_days += days
+            all_dates += dates
+            all_amounts += amounts
+            pyplot.plot(days, amounts, marker='x', markerfacecolor=(0.01*i,0.02*i,0.03*i), linewidth=0.5, markersize='7', label=member['name'])
+        if regBook.ask('-1') != 0:
+            print('\nWarning: Silent RegBook did not exit correctly')
+        days_to_show, dates_to_show = self.dates_list_to_plot_ticks(sorted(all_days), sorted(all_dates))
+        pyplot.xticks(days_to_show, dates_to_show)
+        pyplot.yticks(all_amounts, all_amounts)
+        pyplot.legend()
+        pyplot.show()
 
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--login', default='')
+parser.add_argument('--password', default='')
+parser.add_argument('--token', default='')
+parser.add_argument('--unix', default='')
+command_args = parser.parse_args()
+
+executable = 'RegBook'
+if command_args.unix:
+    executable = './RegBook'
 RegBook(silent=False)
