ccaf551fbae796c329efd25cdf708d97f37a3cbb
Marvel <ilya97@ziza.ru>
2020-05-19 14:38:41 +0300

    VK api integration V2.09 - make regbook connector much faster

--- build/VkManagement/regbookConnector.py
+++ build/VkManagement/regbookConnector.py
@@ -20,7 +20,7 @@ class RegBook:
         thread.daemon = True
         thread.start()
         if silent:
-            self.get_output()
+            self.get_output('Input appropriate number')
         else:
             tools = Tools(command_args)
             self.tools = tools.funcs
@@ -65,13 +65,16 @@ class RegBook:
         while True:
             lines = []
             extra_tools = None
-            while True:
+            counter = 0
+            while counter < 100000:
+                counter += 1
                 try:
-                    line = self.queue.get(timeout=0.03)
+                    line = self.queue.get(timeout=0.1)
                 except Empty:
                     if self.instance.poll() is not None:
                         exit()
                     if len(lines) == 0:
+                        counter += 10000
                         continue
                     if extra_call:
                         self.remove_mistake(lines)
@@ -83,6 +86,10 @@ class RegBook:
                     break
                 else:
                     lines.append(line)
+            if counter >= 100000:
+                self.instance.kill()
+                print('Exiting. Conduit killed due to serious internal error!')
+                exit()
             user_input = input()
             while not user_input:
                 user_input = input()
@@ -93,36 +100,48 @@ class RegBook:
             self.stdin.write(str.encode(user_input + '\n'))
             self.stdin.flush()
 
-    def ask(self, request):
+    def ask(self, request, marker=None):
         self.stdin.write(str.encode(request + '\n'))
         self.stdin.flush()
-        return self.get_output()
+        return self.get_output(marker)
 
-    def get_output(self):
+    def get_output(self, marker=None):
         lines = []
-        while True:
+        marker_detected = False
+        counter = 0
+        while counter < 1000:
             try:
-                line = self.queue.get(timeout=0.05)
+                if marker:
+                    line = self.queue.get(timeout=0.001)
+                    if marker in line:
+                        marker_detected = True
+                else:
+                    line = self.queue.get(timeout=0.1)
             except Empty:
+                if marker and not marker_detected:
+                    counter += 1
+                    continue
                 if self.instance.poll() is not None:
                     return self.instance.returncode
                 return lines
             else:
                 lines.append(line)
+        self.instance.kill()
+        raise RuntimeError('Conduit error on internal request!')
 
     def get_members_list(self, info=True):
         if info:
-            members = self.ask('2')
+            members = self.ask('2', 'Input element name')
         else:
-            members = self.ask('1')
-        self.ask('-1')
-        self.ask('1')
+            members = self.ask('1', 'Input element name')
+        self.ask('-1', 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
         return members
 
     def get_members_amount(self):
-        members = self.ask('2')
-        self.ask('-1')
-        self.ask('1')
+        members = self.ask('2', 'Input element name')
+        self.ask('-1', 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
         amount = 0
         for member in members:
             if len(member.split('\t')) >= 6:
@@ -130,26 +149,26 @@ class RegBook:
         return amount
 
     def get_member_data(self, i):
-        self.ask('2')
-        data = self.ask(str(i))
-        self.ask('1')
+        self.ask('2', 'Input element name')
+        data = self.ask(str(i), 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
         return data
 
     def get_member_data_safe(self, member_id):
-        self.ask('2')
-        data = self.ask(str(member_id))
+        self.ask('2', 'Input element name')
+        data = self.ask(str(member_id), 'Input')
         for line in data:
             if 'Short name' in line:
-                self.ask('1')
+                self.ask('1', 'Input appropriate number')
                 return data
-        self.ask('-1')
-        self.ask('1')
+        self.ask('-1', 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
         return None
 
     def find(self, request):
-        self.ask('11')
-        search_result = self.ask(request)
-        self.ask('1')
+        self.ask('11', 'What do u search for')
+        search_result = self.ask(request, 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
         for line in search_result:
             if 'is made by' in line:
                 return True, line
@@ -157,33 +176,33 @@ class RegBook:
                 return False, None
 
     def add_post(self, member, link, date='0'):
-        self.ask('1')
-        self.ask(member)
-        self.ask(date)
-        self.ask(link)
-        self.ask('1')
+        self.ask('1', 'Or input -1 to return')
+        self.ask(member, 'Input date in format YYYY.MM.DD')
+        self.ask(date, 'Input link')
+        self.ask(link, 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
 
     def get_posts_with_default_status(self):
-        self.ask('8')
-        posts = self.ask('0')[1:-2]
-        self.ask('-1')
-        self.ask('1')
+        self.ask('8', 'Input appropriate number')
+        posts = self.ask('0', 'Input number of the post')[1:-2]
+        self.ask('-1', 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
         return posts
 
     def get_approved_posts(self):
-        self.ask('8')
-        posts = self.ask('2')[1:-2]
-        self.ask('1')
+        self.ask('8', 'Input appropriate number')
+        posts = self.ask('2', 'Input 1 for Yes and 0 for No')[1:-2]
+        self.ask('1', 'Input appropriate number')
         return posts
 
     def approve_post(self, request):
-        self.ask('8')
-        self.ask('0')
-        self.ask(str(request))
-        self.ask('1')
+        self.ask('8', 'Input appropriate number')
+        self.ask('0', 'Input number of the post')
+        self.ask(str(request), 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
 
     def reject_post(self, request):
-        self.ask('8')
-        self.ask('1')
-        self.ask(str(request))
-        self.ask('1')
+        self.ask('8', 'Input appropriate number')
+        self.ask('1', 'Input number of the post')
+        self.ask(str(request), 'Input 1 for Yes and 0 for No')
+        self.ask('1', 'Input appropriate number')
